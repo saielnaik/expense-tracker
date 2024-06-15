de97f3dd46c7cee874957c8a04ad4ccce02b549e
saiel naik <74955042+saielnaik@users.noreply.github.com>
2024-06-15 19:50:41 +0530

    first commit

---
+++ app/_components/Hero.jsx
@@ -0,0 +1,37 @@
+
+import Image from 'next/image';
+
+function Hero() {
+  return (
+    <section className="bg-gray-50 flex items-center flex-col">
+  <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex ">
+    <div className="mx-auto max-w-xl text-center">
+      <h1 className="text-3xl font-extrabold sm:text-5xl">
+        Manage Yor Expense
+        <strong className="font-extrabold text-primary sm:block"> Control Your Money </strong>
+      </h1>
+
+      <p className="mt-4 sm:text-xl/relaxed">
+        Start Creating Your Budget And Save Ton Of Money
+      </p>
+
+      <div className="mt-8 flex flex-wrap justify-center gap-4">
+        <a
+          className="block w-full rounded bg-primary px-12 py-3 text-sm font-medium text-white shadow hover:bg-blue-700 focus:outline-none focus:ring active:bg-primary sm:w-auto"
+          href="#"
+        >
+          Get Started
+        </a>
+      </div>
+    </div>
+  </div>
+  <Image src={'/dashboard.png'} 
+  width={1000}
+  height={700}
+  className='-mt-9 rounded-xl border-2'
+  />
+</section>
+  )
+}
+
+export default Hero
